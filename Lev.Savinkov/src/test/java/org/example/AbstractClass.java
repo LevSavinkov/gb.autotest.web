898d005eabc5f7632479e415866ef7d5fbe6d29e
LevSavinkov <47718635+LevSavinkov@users.noreply.github.com>
2022-02-14 21:56:42 +0400

    Тест2

--- Lev.Savinkov/src/test/java/org/example/AbstractClass.java
+++ Lev.Savinkov/src/test/java/org/example/AbstractClass.java
@@ -1,4 +1,35 @@
 package org.example;
 
-public class AbstractClass {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Before;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
+
+public abstract  class AbstractClass {
+    private static WebDriver driver;
+
+    @BeforeAll
+    static void startDriver () {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--incognito");
+        options.addArguments("disable-popup-blocking");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://www.muztorg.ru/");
+    }
+
+    @AfterAll
+    static void close (){
+        driver.quit();
+    }
+
 }
